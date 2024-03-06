dcff29a3815a641afeea98398427c325c2b485e3
Juan Torres <juan852004da@gmail.com>
2024-03-06 11:17:03 -0500

    Crear array utilizando el constructor Array()

--- index.js
+++ index.js
@@ -6,3 +6,6 @@ let arrayElementos = ["a", "e", "i", "o", "u"];
 
 // Se debe crear un array con elementos diferentes (tipos de datos)
 let arrayElementosDiferentes = [1, "a", hora, 5, "e"];
+
+// Se debe crear un array utilizando el contructor Array();
+let arrayConstructor = new Array();
