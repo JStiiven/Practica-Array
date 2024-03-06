568abd238817cbded3d379158d7a44a5705f3754
Juan Torres <juan852004da@gmail.com>
2024-03-06 11:10:29 -0500

    Crear un array con elementos distintos (tipos de datos)

--- index.js
+++ index.js
@@ -3,3 +3,6 @@ let arrayVacio = [];
 
 // Se debe crear un array con elementos
 let arrayElementos = ["a", "e", "i", "o", "u"];
+
+// Se debe crear un array con elementos diferentes (tipos de datos)
+let arrayElementosDiferentes = [1, "a", hora, 5, "e"];
